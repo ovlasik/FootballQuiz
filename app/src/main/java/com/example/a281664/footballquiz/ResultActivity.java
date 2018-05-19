2e8f23ebc9e1ee800140302765284a789bd2e04c
ovlasik <33056820+ovlasik@users.noreply.github.com>
2018-05-19 11:36:36 +0200

    Add total score

--- app/src/main/java/com/example/a281664/footballquiz/ResultActivity.java
+++ app/src/main/java/com/example/a281664/footballquiz/ResultActivity.java
@@ -1,5 +1,7 @@
 package com.example.a281664.footballquiz;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -12,9 +14,20 @@ public class ResultActivity extends AppCompatActivity {
         setContentView(R.layout.activity_result);
 
         TextView resultLabel = (TextView) findViewById(R.id.resultLabel);
-        TextView totalLabel = (TextView) findViewById(R.id.totalScoreLabel);
+        TextView totalScoreLabel = (TextView) findViewById(R.id.totalScoreLabel);
 
         int score = getIntent().getIntExtra("RIGHT_ANSWER_COUNT", 0);
+
+        SharedPreferences settings = getSharedPreferences("quizApp", Context.MODE_PRIVATE);
+        int totalScore = settings.getInt("totalScore",0);
+        totalScore = totalScore + score;
+
         resultLabel.setText(score + " / 5");
+        totalScoreLabel.setText("Total Score : " + totalScore);
+
+        //update total score
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putInt("totalScore", totalScore);
+        editor.commit();
     }
 }
