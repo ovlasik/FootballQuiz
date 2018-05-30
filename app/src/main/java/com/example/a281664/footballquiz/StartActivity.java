9042e0b5a36fbbfb932b4a0e5a48bccec701e407
ovlasik <33056820+ovlasik@users.noreply.github.com>
2018-05-30 19:01:27 +0200

    Add graphics and music button with music

--- app/src/main/java/com/example/a281664/footballquiz/StartActivity.java
+++ app/src/main/java/com/example/a281664/footballquiz/StartActivity.java
@@ -1,6 +1,7 @@
 package com.example.a281664.footballquiz;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,17 +9,12 @@ import android.view.View;
 import android.widget.ImageButton;
 
 public class StartActivity extends AppCompatActivity {
-
-    //private ImageButton startbutton;
-
-
+MediaPlayer mysong;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
-
-       // startbutton = (ImageButton) findViewById(R.id.startbutton);
-
+        mysong = MediaPlayer.create(StartActivity.this,R.raw.music);
     }
 
     public void startQuiz(View view){
@@ -30,4 +26,16 @@ public class StartActivity extends AppCompatActivity {
         //android.os.Process.killProcess(android.os.Process.myPid());
         System.exit(0);
     }
+
+    public void PlayIT(View v){
+        mysong.start();
+    }
+
+    //@Override
+    //otected void onPause()
+    //{
+      //  super.onPause();
+        //mysong.release();
+    //}
+
 }
